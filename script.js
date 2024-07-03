b952bb4c332f11a3080eef77ccfdc6991a67ba2c
PeterPlatis <xxplatispeterxx@gmail.com>
2024-07-03 23:27:02 +0300

    done

---
+++ script.js
@@ -0,0 +1,75 @@
+//
+//time handling
+//
+const time = document.getElementById("time");
+const now = new Date();
+if(now.getMinutes() <= 9) {
+    time.innerHTML = `${now.getHours()}:0${now.getMinutes()}`;
+} else {
+    time.innerHTML = `${now.getHours()}:${now.getMinutes()}`;
+}
+
+//
+//numberpad handling
+//
+const output = document.getElementById("results-div");
+const input = document.getElementById("user-input");
+const clearButton = document.getElementById("clear");
+
+const numbers = {};
+
+[...document.getElementsByClassName("number")].forEach(el => numbers[el.id] = el);
+
+for (const key in numbers) {
+    numbers[key].addEventListener("click", () => addToInput(numbers[key].value));
+};
+
+function addToInput(string) {
+    input.value += string;
+}
+
+//
+//clear 
+//
+clearButton.addEventListener("click", clearAll);
+
+function clearInput() {
+    input.value = "";
+}
+function clearOutput() {
+    output.innerHTML = "";
+}
+
+function clearAll() {
+    clearInput();
+    clearOutput();
+}
+
+//
+//validate input
+//
+const checkButton = document.getElementById("check-btn");
+
+
+
+checkButton.addEventListener("click", () => {    
+    evaluate(input.value);
+    }
+);
+
+function evaluate(string) {
+    if(string === "") {
+        alert("Please provide a phone number");
+        return
+    } else if (isValid(string)) {
+        output.innerHTML = `Valid US number: <br>${input.value}`;
+        clearInput();
+    } else {
+        output.innerHTML = `Invalid US number: <br>${input.value}`;
+        clearInput();
+    }
+}
+
+function isValid(string) {
+    return /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/.test(string);
+}
